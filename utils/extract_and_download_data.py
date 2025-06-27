07a37136041867bcfd1fb0a37e756dead6f088f1
Joachim Bellet <jobellet@gmail.com>
2025-06-27 11:37:34 +0200

    functions to download and unzip data from figshare

---
+++ utils/extract_and_download_data.py
@@ -0,0 +1,37 @@
+import urllib.request
+import zipfile
+import shutil
+import os
+import pandas as pd
+# Function to download file
+def download_figshare_file(code, filename, private_link='', force_download=False):
+    if len(private_link) > 0:
+        link = f'https://figshare.com/ndownloader/files/{code}?private_link={private_link}'
+    else:
+        link = f'https://figshare.com/ndownloader/files/{code}'
+    
+    if (not os.path.exists(filename)) or force_download:
+        print(f"Downloading {filename}...")
+        urllib.request.urlretrieve(link, filename)
+    else:
+        print(f"{filename} already exists. Skipping download.")
+
+def download_files(path_to_repo, files_to_download, private_link='', force_download=False):
+    df = pd.read_csv(os.path.join(path_to_repo,"file_code_mapping.csv"))
+    # Create a download folder
+    os.makedirs("downloads", exist_ok=True)
+
+    # Download files
+    for _, row in df.iterrows():
+        filename = row['File Name']
+        if filename not in files_to_download:
+            continue
+        code = str(row['Code']).strip()
+        if code != "" and code.lower() != "nan":
+            full_path = os.path.join("downloads", filename)
+            download_figshare_file(code, full_path, private_link=private_link)
+
+# Function to unzip file
+def unzip(zip_path, extract_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
